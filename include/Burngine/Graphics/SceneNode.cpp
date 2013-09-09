d334771e4c96d40b3011d8ee378e0079b705f3b9
FrischerHering <frischer-hering@gmx.de>
2013-09-09 18:19:41 +0200

    added class SceneNode

---
+++ include/Burngine/Graphics/SceneNode.cpp
@@ -0,0 +1,21 @@
+/*
+ * SceneNode.cpp
+ *
+ *  Created on: 09.09.2013
+ *      Author: Dominik
+ */
+
+#include "SceneNode.h"
+
+namespace burn {
+
+SceneNode::SceneNode() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SceneNode::~SceneNode() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace burn */
---
+++ include/Burngine/Graphics/SceneNode.h
@@ -0,0 +1,25 @@
+/*
+ * SceneNode.h
+ *
+ *  Created on: 09.09.2013
+ *      Author: Dominik
+ */
+
+#ifndef SCENENODE_H_
+#define SCENENODE_H_
+
+#include "../Export.h"
+
+namespace burn {
+
+class BURNGINE_API SceneNode {
+public:
+	SceneNode();
+	virtual ~SceneNode();
+
+
+
+};
+
+} /* namespace burn */
+#endif /* SCENENODE_H_ */
