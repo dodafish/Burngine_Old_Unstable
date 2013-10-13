3dc9b2d4e77f5a8b57afcdf9a2587214598b9442
FrischerHering <frischer-hering@gmx.de>
2013-10-13 17:30:02 +0200

    Added ReferenceCounter class
    For getting rid of those ugly pointers avoiding destructorcalls

---
+++ include/Burngine/System/ReferenceCounter.cpp
@@ -0,0 +1,21 @@
+/*
+ * ReferenceCounter.cpp
+ *
+ *  Created on: 13.10.2013
+ *      Author: Dominik
+ */
+
+#include "ReferenceCounter.h"
+
+namespace burn {
+
+ReferenceCounter::ReferenceCounter() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ReferenceCounter::~ReferenceCounter() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace burn */
---
+++ include/Burngine/System/ReferenceCounter.h
@@ -0,0 +1,20 @@
+/*
+ * ReferenceCounter.h
+ *
+ *  Created on: 13.10.2013
+ *      Author: Dominik
+ */
+
+#ifndef REFERENCECOUNTER_H_
+#define REFERENCECOUNTER_H_
+
+namespace burn {
+
+class ReferenceCounter {
+public:
+	ReferenceCounter();
+	~ReferenceCounter();
+};
+
+} /* namespace burn */
+#endif /* REFERENCECOUNTER_H_ */
