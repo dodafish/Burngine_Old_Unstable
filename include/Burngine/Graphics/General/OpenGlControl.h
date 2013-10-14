2b55d18b532feb2d77b1e106996a420429ccb580
FrischerHering <frischer-hering@gmx.de>
2013-10-14 14:29:10 +0200

    Created OpenGlControl

---
+++ include/Burngine/Graphics/General/OpenGlControl.h
@@ -0,0 +1,17 @@
+/*
+ * OpenGlControl.h
+ *
+ *  Created on: 14.10.2013
+ *      Author: Dominik
+ */
+
+#ifndef OPENGLCONTROL_H_
+#define OPENGLCONTROL_H_
+
+namespace burn {
+
+class OpenGlControl {
+};
+
+} /* namespace burn */
+#endif /* OPENGLCONTROL_H_ */
---
+++ src/Burngine/Graphics/General/OpenGlControl.cpp
@@ -0,0 +1,12 @@
+/*
+ * OpenGlControl.cpp
+ *
+ *  Created on: 14.10.2013
+ *      Author: Dominik
+ */
+
+#include <Burngine/Graphics/General/OpenGlControl.h>
+
+namespace burn {
+
+} /* namespace burn */
