f004fa251bd5b5b5a381a7d90e2de36a8fcb206a
FrischerHering <frischer-hering@gmx.de>
2013-10-17 14:18:43 +0200

    Added class RectangleShape

---
+++ include/Burngine/Graphics/Gui/2D/RectangleShape.h
@@ -0,0 +1,29 @@
+/*
+ * RectangleShape.h
+ *
+ *  Created on: 17.10.2013
+ *      Author: Dominik
+ */
+
+#ifndef RECTANGLESHAPE_H_
+#define RECTANGLESHAPE_H_
+
+#include <Burngine/Export.h>
+#include <Burngine/System/Math.h>
+#include <Burngine/Graphics/Gui/GuiNode.h>
+
+namespace burn {
+
+class BURNGINE_API RectangleShape : public GuiNode {
+public:
+	void setDimensions(const Vector2f& dimensions);
+	const Vector2f& getDimensions() const;
+
+	void draw();
+
+private:
+	Vector2f _dimensions;
+};
+
+} /* namespace burn */
+#endif /* RECTANGLESHAPE_H_ */
---
+++ src/Burngine/Graphics/Gui/2D/RectangleShape.cpp
@@ -0,0 +1,19 @@
+/*
+ * RectangleShape.cpp
+ *
+ *  Created on: 17.10.2013
+ *      Author: Dominik
+ */
+
+#include <Burngine/Graphics/Gui/2D/RectangleShape.h>
+#include <Burngine/Graphics/General/VertexBufferObject.h>
+
+namespace burn {
+
+void RectangleShape::draw(){
+
+
+
+}
+
+} /* namespace burn */
