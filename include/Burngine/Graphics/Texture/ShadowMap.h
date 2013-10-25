4a3627aa6defc0094c94f326be0f0938b0dfef38
FrischerHering <frischer-hering@gmx.de>
2013-10-25 15:22:11 +0200

    created class ShadowMap

---
+++ include/Burngine/Graphics/Texture/ShadowMap.h
@@ -0,0 +1,26 @@
+/*
+ * ShadowMap.h
+ *
+ *  Created on: 25.10.2013
+ *      Author: Dominik
+ */
+
+#ifndef SHADOWMAP_H_
+#define SHADOWMAP_H_
+
+#include <Burngine/Export.h>
+#include <Burngine/Graphics/Texture/BaseTexture.h>
+
+namespace burn {
+
+class BURNGINE_API ShadowMap : public BaseTexture {
+public:
+	ShadowMap();
+	~ShadowMap();
+
+private:
+
+};
+
+} /* namespace burn */
+#endif /* SHADOWMAP_H_ */
---
+++ src/Burngine/Graphics/Texture/ShadowMap.cpp
@@ -0,0 +1,18 @@
+/*
+ * ShadowMap.cpp
+ *
+ *  Created on: 25.10.2013
+ *      Author: Dominik
+ */
+
+#include <Burngine/Graphics/Texture/ShadowMap.h>
+
+namespace burn {
+
+ShadowMap::ShadowMap() {
+}
+
+ShadowMap::~ShadowMap() {
+}
+
+} /* namespace burn */
