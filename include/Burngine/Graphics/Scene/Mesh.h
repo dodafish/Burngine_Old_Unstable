9075160041e5d40d24a959dac981719cde64a2d2
FrischerHering <frischer-hering@gmx.de>
2014-01-09 22:01:30 +0100

    Changed year in copyright notice

--- include/Burngine/Graphics/Scene/Mesh.h
+++ include/Burngine/Graphics/Scene/Mesh.h
@@ -3,7 +3,7 @@
 // Burngine is distributed under the GNU GPL v2 License
 // ====================================================
 //
-//    Copyright (C) 2013 Dominik David (frischer-hering@gmx.de)
+//    Copyright (C) 2014 Dominik David (frischer-hering@gmx.de)
 //
 //    This program is free software; you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
